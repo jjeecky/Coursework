0209840006e6ae75aba857bf7ea5f36b33b44f56
jjeecky <146842496+jjeecky@users.noreply.github.com>
2023-11-17 14:55:32 +0000

    Coursework update

---
+++ Coursework1/StepsTask1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Define any additional variables here
+
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() {
+    FITNESS_DATA fd[59];
+    int buffer_size = 100;
+    char line_buffer[buffer_size];
+    int counter = 0;
+    char filename [] = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("");
+        return 1;
+    }
+    
+    char date[11];
+    char time[6];
+    char steps[20];
+    
+    while (fgets (line_buffer, buffer_size, file) != NULL) {
+        tokeniseRecord(line_buffer, ",", date, time, steps);
+        strcpy(fd[counter].date, date);
+        strcpy(fd[counter].time, time);
+        fd[counter].steps = atoi(steps);
+        counter ++;
+    }   
+    printf("Number of records in file: %d\n", counter);
+    for(int i = 0; i < 3; i++) {
+        printf("%s/%s/%d\n", fd[i].date, fd[i].time, fd[i].steps);
+    }
+    return 0;
+
+}
---
+++ Coursework2/StepCounter_Final.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "FitnessDataStruct.h"
+
+// Struct moved to header file
+
+// Define any additional variables here
+// Global variables for filename and FITNESS_DATA array
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+                    }
+
+
+
+
+// Complete the main function
+int main() {
+    while(1){
+        printf("A: Specify the filename to be imported – you need to check that the file opened correctly.\n B: Display the total number of records in the file\n C: Find the date and time of the timeslot with the fewest steps\n D: Find the data and time of the timeslot with the largest number of steps\n E: Find the mean step count of all the records in the file\n F: Find the longest continuous period where the step count is above 500 steps\n Q: Exit "");
+    }
+        
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
